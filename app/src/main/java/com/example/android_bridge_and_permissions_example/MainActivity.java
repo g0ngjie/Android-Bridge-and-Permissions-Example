72314bf1cd0829107f43beda8e62f697e4b03d27
Gj <gongjie0422@163.com>
2022-12-12 18:02:00 +0800

    fix: navigation

--- app/src/main/java/com/example/android_bridge_and_permissions_example/MainActivity.java
+++ app/src/main/java/com/example/android_bridge_and_permissions_example/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.android_bridge_and_permissions_example;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -21,27 +18,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        View btn_login = findViewById(R.id.btn_login);
-//        btn_login.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                System.out.println(v);
-//            }
-//        });
-
-        // 获取页面上的底部导航栏控件
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-
-        // 配置navigation与底部菜单之间的联系
-        // 底部菜单的样式里面的item里面的ID与navigation布局里面指定的ID必须相同，否则会出现绑定失败的情况
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_bridge,R.id.navigation_permission)
-                .build();
-        // 建立fragment容器的控制器，这个容器就是页面的上的fragment容器
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-
-        // 启动
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        View btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println(v);
+            }
+        });
     }
 }
